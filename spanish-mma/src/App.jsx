2c08be8a6cce25e6df64c5e89485b4613fae877f
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-07-26 16:34:34 +0200

    added animations to logo

--- spanish-mma/src/App.jsx
+++ spanish-mma/src/App.jsx
@@ -11,21 +11,24 @@ function App() {
 
             {/* HEADER */}
             <header id='header' className='h-24 bg-custom-red text-white font-sans'>
-                <div className='container mx-auto flex flex-row bebas'>
+                <div className='container w-11/12 mx-auto flex flex-row bebas'>
 
 
                     <div id='logo' className='flex-1'>
 
                         <div className='w-64 mt-1.5 pt-0.5 pb-0.5 bg-custom-gold text-custom-black text-center rounded-md
-                        hover:bg-gray-400 tracking-wider cursor-pointer overflow-hidden transition duration-300
-                        flex flex-row'>
+                        hover:bg-gray-400 hover:text-custom-red tracking-wider cursor-pointer overflow-hidden transition duration-300
+                        flex flex-row group'>
 
-                            <span className='block float-left ml-11 mt-5'>
+                            <span className='block float-left ml-11 mt-5 animation group-hover:scale-125
+                            group-hover:animate-from-bellow'>
                                 <img src='/octagono.png' />
                             </span>
 
-                            <h3 className='block text-3xl mt-1.5 ml-7 float-right mr-16 transition duration-100'>
-                                Spanish MMA
+                            <h3 className='block text-3xl mt-1.5 ml-7 float-right mr-16 transition duration-100
+                            group-hover:animate-from-right'>
+                                Sp<span className='group-hover:text-custom-gold'>ani</span>sh 
+                                M<span className='group-hover:text-custom-gold'>M</span>A
                             </h3>
                         </div>
 
--- spanish-mma/tailwind.config.js
+++ spanish-mma/tailwind.config.js
@@ -20,6 +20,22 @@ export default {
         trebuchet: ['TrebuchetMS', 'sans-serif'],
         symbols: ['WebSymbolsRegular'],
       },
+      animation:{
+        'spin-slow':'spin 7s linear infinite',
+        'from-bellow': 'fromBellow 500ms linear',
+        'from-right': 'fromRight 300ms linear',
+      },
+      keyframes:{
+        fromBellow:{
+          '0%': { transform: 'translateY(0%)' },
+          '50%': { transform: 'translateY(200%)' },
+          '100%': { transform: 'translateY(0%)' },
+        },
+        fromRight:{
+          '0%': { transform: 'translateX(200%)' },
+          '100%': { transform: 'translateX(0%)' },
+        }
+      }
     },
   },
   plugins: [],

d5a2346c6d3678c4194ac91e74d2e8ce1bbc49c6
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-07-28 17:01:26 +0200

    fixed responsive to banner

--- spanish-mma/src/App.jsx
+++ spanish-mma/src/App.jsx
@@ -64,7 +64,7 @@ function App() {
 
                 {/* INICIO BANNER */}
                 <div id='banner' className='h-52 w-full border-8 border-white mx-auto overflow-hidden shadow m-5
-             bg-banner bg-cover bg-no-repeat animate-bg-banner'>
+             bg-banner bg-cover bg-no-repeat animate-bg-banner xs:h-20 md:h-28 lg:h-40'>
 
                 </div>
                 {/* FIN BANNER */}
